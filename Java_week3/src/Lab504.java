8e3b9fc5ebc2ec1efbd18388d9d17431daa3567d
PhakinK <118416994+PhakinK@users.noreply.github.com>
2022-12-08 11:40:56 +0700

    Lab504 ข้อ 4

---
+++ Java_week3/src/Lab504.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Lab504 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Plesae enter your name, Seperate by a space. \n :");
+		String fullName = scanner.nextLine();
+		
+		System.out.println(abbreviatName(fullName) + fullName.substring(0, fullName.indexOf(" ")));
+	}
+	
+	/*
+	private static String abbreviatName(String fullName) {
+		String[] space = fullName.split(" ");
+		
+		return space[space.length - 2].toUpperCase().charAt(0) + "." + space[space.length - 1].toUpperCase().charAt(0);
+	} */
+
+	
+	private static String abbreviatName(String fullName) {
+		String newName = "";
+		
+		for (int i = 0; i < fullName.length(); i++) {
+			if (fullName.charAt(i) == ' ') {
+				newName = (newName + fullName.charAt(i+1)).toUpperCase();
+				newName = newName + ".";
+			}
+		}
+		
+		return newName;
+	}
+}
