ff333ab7ba7d4f21fbbc6c63e3aea2cda2f68ef0
PhakinK <118416994+PhakinK@users.noreply.github.com>
2023-02-09 10:53:15 +0700

    commit 1st exercise
    02-09-2023 10:53AM

---
+++ OOP_Concept/src/Cylinder.java
@@ -0,0 +1,29 @@
+
+public class Cylinder extends Circle {
+
+	private double height;
+	
+	public Cylinder(double radius, double height) {
+		super(radius, null);
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	
+	public double getVolume() {
+		return super.getArea() * height;
+	}
+	
+	@Override
+	public double getArea() {
+		return Math.PI * (height + super.getRadius());
+	}
+	
+	@Override
+	public String toString() {
+		return "Cylinder[height=" + height + ",radius=" + getRadius() + "]";
+	}
+}
